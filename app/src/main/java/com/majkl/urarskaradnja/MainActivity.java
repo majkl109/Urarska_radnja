4a96c2bfd14e8bdd762470bb28e122a55f52cbd6
Majkl <majkl.andracic@gmail.com>
2020-03-18 23:18:58 +0100

    Button in xml

--- app/src/main/java/com/majkl/urarskaradnja/MainActivity.java
+++ app/src/main/java/com/majkl/urarskaradnja/MainActivity.java
@@ -3,6 +3,7 @@ package com.majkl.urarskaradnja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void loadSatovi(View view){
+        MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+        lst.setText(dbHandler.loadHandler());
+        SatID.setText("");
+        NazivSata.setText("");
+
     }
 }
